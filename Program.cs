266e5c33b33bac14563aae2ec65449d3cf17f7a3
twinklefingers <rachelweila@gmail.com>
2016-12-12 23:06:12 -0600

    new file structure

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// using System;
+
+// namespace ConsoleApplication
+// {
+//     public class Program
+//     {
+//         public static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello World!");
+//         }
+//     }
+// }
+
+using System;
+
+namespace HelloWorld
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ello World");
+        }
+    }
+}

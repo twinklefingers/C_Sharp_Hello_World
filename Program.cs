85e5f8f34dad4c2b36e4b40d251177dfc76c6833
twinklefingers <rachelweila@gmail.com>
2016-12-17 18:38:45 -0600

    interactive question

--- Program.cs
+++ Program.cs
@@ -1,25 +1,18 @@
-﻿// using System;
-
-// namespace ConsoleApplication
-// {
-//     public class Program
-//     {
-//         public static void Main(string[] args)
-//         {
-//             Console.WriteLine("Hello World!");
-//         }
-//     }
-// }
-
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace HelloWorld
 {
-    class Program 
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ello World");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name);
         }
     }
 }
